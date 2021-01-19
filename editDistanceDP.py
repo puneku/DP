817db9a04e7b52fa43bb6429c403816fe89a48ef
puneku <puneetkumar3031@gmail.com>
2021-01-20 01:18:20 +0530

    brute force approach with TC - O(3**m)

--- editDistanceDP.py
+++ editDistanceDP.py
@@ -5,3 +5,17 @@
         Replace
     All of the operations are of equal cost.'''
 
+# brute force approach
+def editDistance(str1,str2,m,n):
+    if m == 0:
+        return n
+    if n == 0:
+        return m
+    if str1[m-1] == str2[n-1]:
+        return editDistance(str1,str2,m-1,n-1)
+    return 1 + min(editDistance(str1,str2,m,n-1),editDistance(str1,str2,m-1,n),editDistance(str1,str2,m-1,n-1))
+
+str1 = "abcd"
+str2 = "axyzbdc"
+
+print(editDistance(str1,str2,len(str1),len(str2)))
