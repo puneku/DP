e39d67896038ef609855c335a8ed9af016ae08b8
puneku <puneetkumar3031@gmail.com>
2021-01-20 16:21:14 +0530

    Dp approach TC - O(n**2) and SC - O(n**2)

--- editDistanceDP.py
+++ editDistanceDP.py
@@ -19,3 +19,26 @@ str1 = "abcd"
 str2 = "axyzbdc"
 
 print(editDistance(str1,str2,len(str1),len(str2)))
+
+# dp approach - first
+
+def editDistanceDP(str1,str2):
+    m = len(str1)
+    n = len(str2)
+    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i-1][j], # Insert
+                                   dp[i-1][j-1], # Replace
+                                   dp[i][j-1]) # Remove
+    return dp[m][n]
+
+print(editDistanceDP(str1,str2))
+
